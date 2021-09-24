1d91f0a2f9d3a9ce2930f0d885d51d222477dc18
vdudarev <vdudarev@hse.ru>
2021-09-24 23:17:59 +0300

    init

---
+++ ChemicalUnitTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Xml;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IMETlib.Chemistry;
+
+namespace ChimicalHierarchyTest
+{
+    [TestClass]
+    public class ChemicalUnitTest
+    {
+        [TestMethod]
+        public void TestSystem()
+        {
+            XmlNode InXmlNode = null;
+            int NumElements = 0;
+            string mySystemXML = "LiNbO<sub>3</sub>";
+            string mySystemString = "";
+            string resp = "";
+            int ret = REFACTOR_ChemicalUtils.GetSystemStringByDescription(2, ref InXmlNode, ref NumElements, ref mySystemXML, ref mySystemString, ref resp);
+
+            Assert.AreEqual(ret, 0);
+            Assert.AreEqual(mySystemXML, "<ChemicalSystem><Element>Li</Element><Element>Nb</Element><Element>O</Element></ChemicalSystem>");
+            Assert.AreEqual(mySystemString, "-Li-Nb-O-");
+
+        }
+
+
+
+        [TestMethod]
+        public void TestSubstance()
+        {
+            string html = "LiNbO<sub>3</sub>";
+            string substanceXML;
+            string substanceString = REFACTOR_ChemicalUtils.GetSubstanceStringByHtml(html, out substanceXML);
+            
+            Assert.AreEqual(substanceXML, "<ChemicalSubstanceComposition><Item Element=\"Li\" value=\"1\" /><Item Element=\"Nb\" value=\"1\" /><Item Element=\"O\" value=\"3\" /></ChemicalSubstanceComposition>");
+            Assert.AreEqual(substanceString, "Li(1)Nb(1)O(3)");
+        }
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ChimicalHierarchyTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ChimicalHierarchyTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9172c410-06c7-4c37-8a42-0d6230001ac8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
